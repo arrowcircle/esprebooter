7a657c0c6ab456e4fe2c238f8dbad4856801c3af
Oleg Bovykin <oleg.bovykin@gmail.com>
2023-10-07 11:44:31 +0200

    Set 2 min delay between attempts

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include "WiFi.h"
 #include <string>
 #include <vector>
+#include "esp_sleep.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
 
-#define RELAY_PIN 25
-#define NETWORK String("Ivy Knob1")
+#define RELAY_PIN 8
+#define NETWORK String("Ivy Knob")
 #define uS_TO_S_FACTOR 1000000
 // Uncomment for development | Comment for production
-#define SUCCESSFUL_SLEEP_DURATION 60 // 24 * 60 * 60 // 24 hours
-#define FAILED_SLEEP_DURATION 15 // 60 * 60 // 1 hour
+#define SUCCESSFUL_SLEEP_DURATION 60 * 60 // 60 * 60 // 1 hour
+#define FAILED_SLEEP_DURATION 60 * 60 // 15 // 60 * 60 // 1 hour
+#define ATTEMPT_DURATION 2 * 60 // 2 mins
 // Uncomment for production | Comment for development
 // #define SUCCESSFUL_SLEEP_DURATION 24 * 60 * 60 // 24 hours
 // #define FAILED_SLEEP_DURATION 60 * 60 // 1 hour
@@ -46,6 +50,9 @@ void long_sleep(int seconds) {
   disable_wifi();
   esp_sleep_enable_timer_wakeup(seconds * uS_TO_S_FACTOR);
   Serial.flush();
+  digitalWrite(RELAY_PIN, LOW);
+  gpio_hold_en(GPIO_NUM_8);
+  gpio_deep_sleep_hold_en();
   esp_deep_sleep_start();
   // This will never run
   ESP.restart();
@@ -53,6 +60,8 @@ void long_sleep(int seconds) {
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
+  gpio_hold_dis(GPIO_NUM_8);
+  digitalWrite(RELAY_PIN, LOW);
   Serial.begin(115200);
   disable_wifi();
   Serial.println("Setup done");
@@ -72,12 +81,12 @@ void loop() {
     Serial.printf("Maximum attempts reached: %d. Restarting router.\n", MAX_ATTEMPTS);
     // connect pin
     digitalWrite(RELAY_PIN, HIGH);   // Turn on relay
-    delay(1000);
+    delay(2000); 
     // disconnect pin
     digitalWrite(RELAY_PIN, LOW);    // Turn off relay
     // sleep for 1 hour
     long_sleep(FAILED_SLEEP_DURATION);
   }
   // Wait a bit before scanning again.
-  delay(10000);
+  delay(ATTEMPT_DURATION * 1000);
 }
